d14d0370c4c60c8a864b2fd43ba8c68e09031171
Daniel Santiago Pérez <danielsantiagoperez03@gmail.com>
2024-05-21 17:00:41 -0500

    Prev and next page buttons added

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,20 +1,57 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component( {
   selector: 'list-elements',
   standalone: true,
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
-  imports: [ MatTableModule, MatPaginatorModule ]
+  imports: [ MatTableModule, MatPaginatorModule, MatButtonModule ]
 } )
 
 export class ListComponent implements OnInit, OnChanges {
+  constructor (
+    private router: Router
+  ) {}
+
   // Props
   @Input() tableName: string = 'Tabla de Datos';
+  @Input() gotoItems: string = '';
   @Input() tableData: any[] = [];
   @Input() columns: any[] = [];
+  @Input() prevButton: () => void = () => { console.log( 'Click on prevButton!' ) };
+  @Input() nextButton: () => void = () => { console.log( 'Click on nextButton!' ) };
+
+  // Click events
+  prevButtonEvent () { this.prevButton() }
+  nextButtonEvent () { this.nextButton() }
+  onRowClick ( row: any ) {
+    if ( ( this.gotoItems ) ) {
+      const ids = row[ this.gotoItems ].map( ( item: any ) => {
+        const arr = item.replaceAll( '/', ' ' ).trim().replaceAll( ' ', ',' ).split( ',' );
+        const size = arr.length - 1;
+
+        return( arr[ size ] );
+      } ).join( ',' );
+
+      const equival: { [key: string]: string } = {
+        vehicles: 'vehiculos',
+        residents: 'residentes'
+      }
+
+      const pageToGo = equival[ this.gotoItems ];
+
+      const queryParams = {
+        ids: ids
+      };
+
+      this.router.navigate( [ `/${pageToGo}` ], { queryParams } );
+    }
+  }
+
 
   // Local props
   displayedColumns: string[] = [];
