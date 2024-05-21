42b35f24a97cbd79a4fb9254c63e51b720400d24
Daniel Santiago Pérez <danielsantiagoperez03@gmail.com>
2024-05-20 23:14:21 -0500

    Cleaned and prepared some files to customize the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
+@Component( {
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+  styleUrl: './app.component.css',
+  imports: [ RouterOutlet ]
+} )
+
 export class AppComponent {
   title = 'prueba-tecnica-angular';
 }
